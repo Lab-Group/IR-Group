aa74cd8e0d58f0290a69e82adefd593a358feab1
Lab-Group <labgroup86@gmail.com>
2026-04-26 02:57:49 +0200

    IR Project HW1 + HW2 completed

--- HW1/Create_Index.py
+++ HW1/Create_Index.py
@@ -1,51 +1,46 @@
+from bs4 import BeautifulSoup
 import time
 from elasticsearch import Elasticsearch
-from bs4 import BeautifulSoup
-import os
-import re
-from collections import Counter
-import dill
 
-es = Elasticsearch()
-path = "/Users/Zion/Downloads/AP_DATA/ap89_collection/"
+es = Elasticsearch(
+    "https://localhost:9200",
+    basic_auth=("elastic", "a8t9_j71q0kE7upr0*i-"),
+    verify_certs=False
+)
+
+path = r"C:\Users\fikir\Downloads\Telegram Desktop\cranfield-trec-dataset-main\cranfield-trec-dataset-main\cran.all.1400.xml"
+
 start_time = time.time()
+
+with open(path, "r", encoding="utf-8", errors="ignore") as file:
+    data = file.read()
+
+# wrap in root because XML is not always single-rooted
+soup = BeautifulSoup(data, "xml")
+
+docs = soup.find_all("doc")
+
 i = 0
-for filename in os.listdir(path):
-    #if (filename == 'ap890130'):
-    if(filename != 'readme'):
-        file = open(path+filename)
-        page = file.read()
-        #docNo = []
-        validPage = "<root>" + page + "</root>"
-        soup = BeautifulSoup(validPage, 'xml')
-        docs = soup.find_all('DOC')
-        for doc in docs:
 
-            i+=1
-            texts = doc.find_all('TEXT')
-            text = ""
-            for txt in texts:
-                text += txt.get_text()
-            count = 0
-            for line in text.splitlines():
-                word = re.sub('\s+', ' ', line).strip().split(' ')
-                count += len(word)
-            jsonDoc = {
-                    'text': text
-            }
-            #docNo.append(doc.DOCNO.get_text().strip())
-            res = es.index(index="index1", doc_type='document', id=doc.DOCNO.get_text().strip(), body=jsonDoc)
-            print("Indexed %d document" % i)
-# f = open("Pickles/docIDs.txt", "wb")
-# dill.dump(docNo, f)
-# f.close()
+for doc in docs:
+    doc_id = doc.find("docno")
+    text = doc.get_text(separator=" ")
 
-temp = time.time()-start_time
-print(temp)
-hours = temp//3600
-temp = temp - 3600*hours
-minutes = temp//60
-seconds = temp - 60*minutes
-print('%d:%d:%d' %(hours,minutes,seconds))
+    if doc_id:
+        jsonDoc = {
+            "text": text
+        }
 
+        es.index(
+            index="cranfield_index",
+            id=doc_id.text.strip(),
+            document=jsonDoc
+        )
 
+        i += 1
+        print("Indexed:", i)
+
+print("Done indexing")
+
+end_time = time.time()
+print("Time:", end_time - start_time)
