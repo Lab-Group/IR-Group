1f4f25726da8af13a197291ed12f37bb49227613
Yeabsira <your_email@example.com>
2026-05-01 21:50:32 +0300

    Update Indexer.py for HW7

--- HW7/Indexer.py
+++ HW7/Indexer.py
@@ -2,41 +2,35 @@ import time
 from elasticsearch import Elasticsearch
 from bs4 import BeautifulSoup
 import os
-import re
-from collections import Counter
-import dill
 
-es = Elasticsearch()
+es = Elasticsearch(
+    "https://localhost:9200",
+    basic_auth=("elastic", "1R4Nfr=txQa1SdVxU3gh"),
+    verify_certs=False
+)
 path = "Files/"
 start_time = time.time()
 i = 0
+
 for filename in os.listdir(path):
-    if(filename != '.DS_Store'):
-        print(filename)
-        file = open(path+filename, "r", encoding="ISO-8859-1")
-        page = file.read()
+    filepath = os.path.join(path, filename)
+    if not os.path.isfile(filepath):
+        continue
+    try:
+        with open(filepath, "r", encoding="utf-8") as file:
+            page = file.read()
         validPage = "<root>" + page + "</root>"
         soup = BeautifulSoup(validPage, 'html.parser')
+        text = soup.find('text').get_text().strip()
+        label = soup.find('label').get_text().strip()
+        emailid = soup.find('emailid').get_text().strip()
+        jsonDoc = {'text': text, 'label': label}
+        es.index(index="hw7_index", id=emailid, body=jsonDoc)
         i += 1
-        texts = soup.find_all('text')
-        labels = soup.find_all('label')
-        text = ""
-        label = ""
-        for txt in texts:
-            text += txt.get_text().strip()
-        for l in labels:
-            label += l.get_text().strip()
-        jsonDoc = {
-            'text': text,
-            'label': label
-        }
-        res = es.index(index="hw7_index", doc_type='document', id=soup.emailid.text.strip(), body=jsonDoc)
-        print("Indexed %d document" % i)
+        print("Indexed %d: %s" % (i, filename))
+    except Exception as e:
+        print("Skipped %s: %s" % (filename, e))
 
 temp = time.time() - start_time
-print(temp)
-hours = temp // 3600
-temp = temp - 3600 * hours
-minutes = temp // 60
-seconds = temp - 60 * minutes
-print('%d:%d:%d' % (hours, minutes, seconds))
+print("Done! Total indexed: %d" % i)
+print("Time: %d:%d:%d" % (temp // 3600, (temp % 3600) // 60, temp % 60))
