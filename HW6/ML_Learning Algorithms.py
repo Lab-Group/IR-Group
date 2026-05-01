ab5e67ddc72c2b682a3447cc8323427a927095bb
Yeabsira <your_email@example.com>
2026-05-01 20:13:16 +0300

    Update ML_Learning Algorithms.py for HW6

--- HW6/ML_Learning Algorithms.py
+++ HW6/ML_Learning Algorithms.py
@@ -3,8 +3,6 @@ from sklearn import model_selection
 from sklearn import linear_model
 from operator import itemgetter
 
-from operator import itemgetter
-
 def createDict(qdIDTest, predictions):
     testDict = {}
     i = 0
@@ -22,44 +20,43 @@ def createDict(qdIDTest, predictions):
 
 def sortDict(testDict):
     for item in testDict:
-        sorted_list = sorted(testDict[item], key=itemgetter(1))
+        sorted_list = sorted(testDict[item], key=itemgetter(1), reverse=True)
         testDict[item] = sorted_list
     return testDict
 
 def createPerformanceFile(testDict):
     with open('trainingperformance.txt', 'a') as f:
-        i = 0
         for item in testDict:
             i = 0
             for docScorePair in testDict[item]:
-                i+=1
-                f.write("%s Q0 %s %d %f Exp\n" %(item, docScorePair[0], i, docScorePair[1]))
-    f.close()
+                i += 1
+                f.write("%s Q0 %s %d %f Exp\n" % (item, docScorePair[0], i, docScorePair[1]))
 
 def main():
     qID_docID = ['QID-DocID']
 
-    df = pandas.read_csv('staticFeatureMatrix.csv', skipinitialspace=True, usecols=qID_docID) #contains query ID and doc ID
+    df = pandas.read_csv('staticFeatureMatrix.csv', skipinitialspace=True, usecols=qID_docID)
     df_array = df.values
     qdID = df_array
 
     names = ['TF-IDF', 'Okapi TF', 'BM25', 'Laplace', 'Jelinek-Mercer', 'Label']
-    dataset = pandas.read_csv('staticFeatureMatrix.csv', names=names, skiprows=[0]) #contains retrieval model scores and label
+    dataset = pandas.read_csv('staticFeatureMatrix.csv', names=names, skiprows=[0])
+    dataset = dataset.fillna(0)
     array = dataset.values
-    X = array[:,0:5] #contains only retrieval model scores
-    Y = array[:,5]#contains only label
+    X = array[:, 0:5]
+    Y = array[:, 5]
 
-    kfold = model_selection.KFold(n_splits=5, random_state=None, shuffle=False) #k-fold cross validation
+    kfold = model_selection.KFold(n_splits=5, random_state=None, shuffle=False)
     for train, test in kfold.split(X, Y):
         qdIDTest = qdID[train]
         regr = linear_model.LinearRegression()
-        regr.fit(X[train], Y[train]) #train
-        predictions = regr.predict(X[train]) #predict
-        # print(len(predictions))
+        regr.fit(X[train], Y[train])
+        predictions = regr.predict(X[train])
 
         testDict = createDict(qdIDTest, predictions)
         testDict = sortDict(testDict)
         createPerformanceFile(testDict)
 
-main()
+    print("Done! trainingperformance.txt created.")
 
+main()
